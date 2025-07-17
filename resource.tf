9ea5e535646c36baa5984ebd4fcc6381fb60e73d
tejasri0312 <ttslakshmi03@gmail.com>
2025-07-17 16:28:10 +0000

    firstcommit

--- resource.tf
+++ resource.tf
@@ -1,4 +1,7 @@
-resource "azurerm_resource_group" "terratask" {
-  name     = "terratask"
-  location = "East US"
+resource "azurerm_storage_account" "saterra" {
+    name                     = "terrastorageaccount0312"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
 }
