f68161ffae78877bf4ecd5ee617771ec6bf0cc01
tejasri0312 <ttslakshmi03@gmail.com>
2025-07-17 16:42:37 +0000

    firstcommit

--- resource.tf
+++ resource.tf
@@ -1,7 +1,5 @@
-resource "azurerm_storage_account" "saterra" {
-    name                     = "terrastorageaccount0312"
-    resource_group_name      = terratask.storeterra.name
-    location                 = azurerm_terratask.storeterra.location
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
 }
